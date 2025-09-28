24939c35e3226b6ecf027a7446396766f59f9555
Artur <artur.czerwiak@gmail.com>
2025-09-28 16:12:45 +0200

    Task 03 completed

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,11 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+while (randomNumber !== x) {
+	randomNumber = Math.round(Math.random() * x);
+	console.log('Random number: ' + randomNumber);
+	iteration++;
+}
+
+console.log('Iterations: ' + iteration);
