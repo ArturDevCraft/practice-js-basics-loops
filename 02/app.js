e83b79566892acd0f532cd0c80ef097f02de0731
Artur <artur.czerwiak@gmail.com>
2025-09-28 15:54:18 +0200

    Task 02 completed

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,16 @@
 const x = 5;
 let result = 0;
+
+for (let i = 0; i <= 5; i++) {
+	result += i;
+	console.log(result);
+}
+
+result = 0;
+
+let i = 0;
+while (i <= 5) {
+	result += i;
+	console.log(result);
+	i++;
+}
